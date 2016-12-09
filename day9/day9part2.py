bc71b44a4f34b3fe3cf21185c3b9b561a0b30df0
Pavle Jonoski <jonoski.pavle@gmail>
2016-12-09 08:28:36 +0100

    Day 2, part 2 solution.

--- day9/day9part2.py
+++ day9/day9part2.py
@@ -12,56 +12,46 @@ if 'test' in sys.argv:
 
 decompressed = ''
 
-def count_size(s):
-  l = 0
-  m = False
-  for c in s:
-    if c in ' \t\n\r':
+def count_decompressed(fac, token):
+  tot = 0
+  marker = ''
+  i = 0
+  while i < len(token):
+    c = token[i]
+    if c == '(' and not marker:
+      marker = '('
+    elif c == ')' and marker:
+      """
+      Doing a leap of faith here:
+        - assuming that the part of the string that should be repeated after this
+        marker will always be of the form: (marker)(marker2)...(markerN)SOME_STRING
+        i.e all the markers are consecuitive at the begining of the token.
+      """
+      sub = token[i+1:]
+      marker = marker[1:]
+      marker = marker.split('x')
+      cnt = int(marker[0])
+      rep = int(marker[1])
+      sub = sub[0:cnt]
+      tot += count_decompressed(rep, sub)
+      token = token[i+1+cnt:]
+      i = 0
+      marker = ''
       continue
-    if c == '(' and not m:
-      m = True
-      continue
-    if c == ')' and m:
-      m = False
-      continue
-    if not m:
-      l += 1
-  return l
+    elif marker:
+      marker += c
+    else:
+      if c not in ' \n\r\t':
+        tot += 1
+    i += 1
+      
+      
+  return tot*fac
 
 total = 0
 gc = 0
 with open(inp_file) as f:
   compressed = f.read()
-  marker = ''
-  i = 0
-  while i < len(compressed):
-    c = compressed[i]
-    if c == '(' and not marker:
-      if not marker:
-        marker = '('
-    elif c == ')' and marker:
-      marker = marker[1:]
-      marker = marker.split('x')
-      cnt = int(marker[0])
-      repeat = int(marker[1])
-      chunk = compressed[i+1: i+cnt+1]
-      chunk = ''.join([chunk for j in range(0, repeat)])
-      compressed = chunk +compressed[i+1+cnt:]
-      #print(' [c]>', chunk)
-      #print(' [s]>', compressed)
-      i = 0
-      marker = ''
-      #print(' [t]>', total)
-      if gc % 1000 == 0:
-        print(' >', len(compressed), ' [t]>', total)
-      continue
-    else:
-      if not marker:
-        if c not in ' \n\r\t':
-          total += 1
-      else:
-        marker += c
-    i += 1
-    gc += 1
+  total = count_decompressed(1, compressed)
 
 print (total)
